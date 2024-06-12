c03becaed816af5a67c218eadd900847acd228da
yanil8068 <yanil8068@gmail.com>
2024-06-13 03:36:53 +0530

    updated url

--- client/src/utilities/baseUrl.js
+++ client/src/utilities/baseUrl.js
@@ -1,4 +1,4 @@
 // const baseUrl = "http://localhost:8000";
-const baseUrl = "https://entertainment-app-jags.vercel.app/api";
+const baseUrl = "https://entertainment-app-sable.vercel.app/api";
 
 export default baseUrl;
